ff51f60eb533503b654a67a1acb2cd9ea8c0b159
AnshSonone <sononeansh@gmail.com>
2025-03-30 14:11:00 +0530

    fix typeError

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig(({mode}) => {
-  const env = process.env(mode, process.cwd())
+  const env = loadEnv(mode, process.cwd())
 
   return {
     plugins: [
